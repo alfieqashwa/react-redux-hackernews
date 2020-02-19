117facd5e3ccc2457e60beaf64cee3304317eeea
alfieqashwa <alfieqashwa@gmail.com>
2020-02-19 18:06:09 +0700

    its working

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./Story.css";
 
 import { connect } from "react-redux";
-import { doArchiveStory } from "../actions/archive";
 import { getReadableStories } from "../selectors/story";
 
 import Story from "./Story";
--- src/index.js
+++ src/index.js
@@ -10,25 +10,6 @@ import { doArchiveStory } from "./actions/archive";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-const stories = [
-  {
-    title: "React",
-    url: "https://facebook.github.io/react/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0
-  },
-  {
-    title: "Redux",
-    url: "https://github.com/reactjs/redux",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1
-  }
-];
-
 ReactDOM.render(
   <Provider store={store}>
     <App
